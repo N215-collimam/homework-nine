32f915a9c8d6ed23a223bbadd8dbaea9b0c6781b
mars908 <78188237+mars908@users.noreply.github.com>
2022-04-11 19:48:16 -0400

    base set up

---
+++ README.md
@@ -0,0 +1,17 @@
+# homework-nine
+## This is an example of using a model.js file with and app.js to create a webpage.
+
+
+Our objectives for this particular webpage were to:
+
+Use jquery for the set up of each webpage
+
+Use a header/ navigation bar and a footer that would appear on each page exactly the same.
+
+Come up with original content to practice mixing jquery with previous knowledge.
+
+Use a model file to better organize the jquery used inside the website.
+
+I made these webpages by first downloading the jquery to my library, and then I made the basic outline for each page. Then, I created the model.js file, and began moving my content over to that file. Then, I tied the model file to the app file. Finally, I created the stylesheets I used for styling the elements of each page.
+
+https://in-info-web4.informatics.iupui.edu/~collimam/homework-nine/
---
+++ model/model.js
@@ -0,0 +1,110 @@
+var homeContent = `<div class="hero"></div>
+
+            
+<div class="content">
+    <h1>BLUE DRAGON GAMES</h1>
+    <h2>HOME</h2>
+    <div class="products">
+        <div class="product">
+        <div class="home-images">
+            <div class="home-image1"></div>
+            <div class="home-image2"></div>
+            <div class="home-image3"></div>
+        </div>
+            <div class="home-desc">
+                <h2>Our Mission</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque voluptate non at quo. Itaque quia ut totam iure placeat sit, excepturi voluptatibus asperiores voluptatum in ab molestias, temporibus earum corrupti.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore eveniet vitae, fugiat distinctio esse dicta accusamus soluta enim, nisi, beatae at vel quod. Dolorum quod, dignissimos fuga vero, sed tempore aspernatur laudantium illo veniam officiis ad molestiae dolorem! Excepturi in quis consequuntur tempora adipisci nostrum ad sed libero obcaecati maxime.</p>
+                <h2>Shop for supplies and games!</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique tempore est ad, deleniti aspernatur obcaecati dolorum perspiciatis dicta animi quas enim hic dolor dignissimos! Sequi aspernatur iste tempore amet, quaerat dolor ipsam voluptates corporis, facilis eum veniam dignissimos cupiditate pariatur? Fugit, aut veritatis!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, aliquam. Recusandae et omnis at dolore tempora, architecto perferendis odio obcaecati.</p>
+                <h2>Schedule an event today!</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat cupiditate culpa quidem, nam cum obcaecati sunt iste ratione id eveniet, vitae eligendi voluptate quia in!</p>
+            </div>
+        </div>
+    </div>
+</div>`;
+
+var aboutContent = `<div class="hero"></div>
+
+<div class="content">
+    <h1>ABOUT</h1>
+    <div class="products">
+        <div class="product">
+            <div class="about-images">
+                <div class="about-image1"></div>
+                <h3>Origin of the blue dragon name</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam et magnam architecto quas saepe voluptates maxime aliquid provident ratione beatae! Beatae fuga nostrum harum rerum ex eaque optio praesentium obcaecati pariatur, vitae consequuntur esse veritatis, totam ad? Perspiciatis, unde sapiente. Necessitatibus recusandae repudiandae ipsam cumque aliquam fuga! Hic, velit. Atque ipsum libero consequatur est eos voluptatem saepe, eveniet enim ut quod animi maiores inventore nihil laudantium laboriosam illo sapiente dicta sequi ratione molestias facilis mollitia? Ipsa architecto magni neque itaque dolorem quasi quidem nostrum quod expedita deserunt, cumque voluptatibus fugit. Voluptas excepturi quaerat facilis veniam praesentium necessitatibus dolores sed suscipit magni velit alias, illum magnam sequi temporibus eligendi minima quos, reiciendis pariatur? Assumenda aperiam libero molestias. Ducimus excepturi nisi nihil nulla.</p>
+                <div class="about-image2"></div>
+                <h3>Meet the owners</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor dignissimos neque necessitatibus voluptate nemo corporis? Dolorum perferendis, illo aperiam excepturi tempore veniam hic molestiae et ducimus veritatis sunt quo temporibus. Soluta ducimus fuga necessitatibus aliquid illum eaque numquam esse eos, nostrum unde. Voluptatum placeat hic, modi magnam sunt tempora totam error saepe eaque quia in eligendi dolorem iure incidunt autem dicta. Animi fugiat quis incidunt culpa natus veritatis praesentium voluptate minima amet! Mollitia, recusandae! Nostrum, debitis provident.</p>
+                <div class="about-image3"></div>
+                <h3>Humble beginnings</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex quia tempore asperiores natus libero aperiam illo? Eligendi neque nisi harum sed, voluptate quibusdam at nulla architecto cumque aut nihil nobis, non magni vel eum veniam mollitia incidunt id sunt eveniet officiis necessitatibus dicta quisquam? Voluptates mollitia accusantium alias sunt? Sint incidunt optio asperiores sit, dolorem iure rerum reiciendis dolore iusto esse dignissimos sequi temporibus, quis distinctio placeat. Accusantium necessitatibus rem tenetur veritatis temporibus vel atque reprehenderit porro excepturi placeat nisi ab, maiores, sed sunt ducimus a dignissimos autem aliquid corporis quas nostrum, itaque saepe tempore. Eos aperiam qui ipsum placeat autem voluptatum sit quibusdam, voluptates cum, deserunt, minima illo dolore itaque quis harum voluptatibus! Quasi doloribus non, accusamus quidem perferendis reprehenderit mollitia consectetur!</p>
+                <div class="about-image4"></div>
+                <h3>Come visit us!</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis cum doloremque dolore non? Ipsam quam harum dicta veritatis voluptates, sequi eaque facilis eveniet quia, exercitationem quae cupiditate nihil facere impedit soluta quo quisquam quos minima. Quaerat repellendus sequi at reiciendis a ad amet, nihil voluptatibus exercitationem repellat sapiente magnam nesciunt incidunt suscipit! Quidem, fuga enim in illo aliquam molestiae esse tempora mollitia accusantium, necessitatibus laborum, ad aperiam!</p>
+            </div>
+                        
+                    </div>
+                </div>
+            </div>`;
+
+var productsContent = `<div class="hero"></div>
+
+<div class="content">
+                <h1>SUPPLIES</h1>
+                <div class="products">
+                        <div class="product">
+                        <div class="supplies">
+                            <div class="supplies-image1"></div>
+                            <div class="supplies-image2"></div>
+                            <div class="supplies-image3"></div>
+                            <div class="supplies-image4"></div>
+                            <div class="supplies-image5"></div>
+                        </div>
+
+                        <div class="prod-desc">
+                        <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore exercitationem, accusamus labore tempora beatae doloremque ipsam sunt nemo modi consequuntur nobis molestias pariatur quisquam suscipit.</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed fuga culpa quia est tenetur, perferendis exercitationem assumenda ex vitae eaque eveniet soluta saepe repudiandae eius molestiae libero doloremque similique in possimus totam atque sunt quas harum? Ipsam, vitae impedit provident ratione iure esse eaque amet voluptatum harum at odit doloribus corrupti sit culpa? Nemo vitae itaque natus repellendus, totam nesciunt soluta. Odio laboriosam harum enim?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque iste doloribus a non eaque dolorum nobis soluta, voluptate provident reiciendis quisquam repellendus qui quo molestias consequuntur sint ab, ducimus quos nostrum officia laudantium corrupti. Amet non quaerat veniam quo sint quis eius aspernatur rem dolores ratione a corporis, deleniti ipsum laboriosam odit! Nisi, ex. Facere adipisci hic fugiat nemo vero accusantium autem voluptatem ratione sequi impedit ipsum eaque officia ut quod temporibus deleniti, sit consectetur, necessitatibus itaque non, quasi iure soluta dignissimos eius! Non praesentium odit suscipit animi. Dicta.</p>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos aliquam beatae, aliquid optio quisquam labore veniam! Cupiditate eos debitis inventore blanditiis accusamus dolore ad reprehenderit nemo perspiciatis consequuntur, porro impedit ipsum maxime enim tempora pariatur? Aliquid, voluptatum. Corrupti illum ab ut dicta tempora?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, qui illum? Qui aut nulla eius voluptas perferendis ipsam, voluptatum culpa voluptate expedita ullam sequi molestias sapiente corrupti praesentium nemo officiis aperiam, alias minus! Porro animi, labore distinctio minus similique culpa obcaecati eveniet dignissimos rerum nesciunt optio, assumenda adipisci nobis beatae? Nesciunt voluptates nulla accusamus necessitatibus quibusdam doloribus dolore laudantium minus quas illum, voluptatum iusto veritatis blanditiis molestias tempore voluptatibus praesentium cum laborum deleniti natus unde alias facilis. Labore quasi adipisci molestias eligendi mollitia. Ea dignissimos enim officiis. Iure commodi ullam facilis laboriosam deleniti! Numquam officia expedita quod sit, deserunt excepturi officiis optio minus, nemo amet distinctio dolorum explicabo provident, at praesentium doloremque sed in inventore. Tempora, assumenda ea! Odit id suscipit maiores. Suscipit, cum dignissimos repellat voluptatum fuga numquam perferendis aliquam, explicabo consectetur qui recusandae provident iste molestiae! Labore, blanditiis non ad mollitia eligendi maiores repellat culpa, nihil perferendis consectetur minus, accusamus rem fugiat recusandae deserunt!</p>
+                        <h3>Pick a picture to get started!</h3>
+                    </div>
+
+                        </div>
+                    </div>
+                </div>`;
+
+var contactContent = `<div class="hero"></div>
+
+<div class="content">
+                <h1>EVENTS</h1>
+                <div class="products">
+                        <div class="product">
+                        <div class="event-images">
+                        <div class="event-image1"></div>
+                        <h3>See the space</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia vel distinctio consectetur fugit nihil placeat deserunt assumenda sunt ea corporis atque odit, repudiandae laborum voluptas blanditiis aliquid vitae repellendus sapiente inventore perspiciatis ab obcaecati soluta cum facilis. Hic iste rem aliquid sequi doloribus at architecto debitis eius alias provident minus aut blanditiis distinctio adipisci deleniti eligendi aliquam, pariatur, ex eaque dolore laboriosam nesciunt itaque harum. Assumenda doloribus velit amet provident ratione at, quos repellendus repellat deserunt, excepturi molestias eligendi praesentium maxime mollitia vero ad ea eveniet dolore ipsam inventore blanditiis libero quae magnam accusantium. Illo neque obcaecati consequuntur assumenda vel nemo commodi nulla iusto autem, ullam facilis? Sapiente modi nam ad eius pariatur corrupti iste, architecto, quod vero quas, saepe repellat iure aliquid. Dolorum magnam in delectus quam eos distinctio totam porro animi temporibus maxime iure, ipsum illum fugiat recusandae dolor sint eius deserunt corrupti inventore sunt officiis, alias autem iusto. Sunt quas ullam nostrum adipisci possimus voluptatibus, asperiores earum pariatur maiores.</p>
+                        <div class="event-image2"></div>
+                        <h3>Weekly events</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sunt dolore ea pariatur necessitatibus, tempore optio, temporibus enim sed reiciendis, inventore recusandae ex culpa ullam eius ipsa porro. Vitae nemo delectus accusamus saepe aspernatur. Corporis impedit, repellendus illum sed doloribus in? Totam unde dolor error cumque iusto nulla molestias nemo ducimus, odit qui nisi neque maiores explicabo blanditiis, voluptates aliquam non nihil eaque ut, tenetur reiciendis ipsum repellat! Eaque minima sunt adipisci dolorem reprehenderit laudantium culpa laborum, numquam veniam enim quam dignissimos facilis tenetur tempore deleniti itaque blanditiis. Illum veniam quod laborum unde veritatis exercitationem sapiente blanditiis quisquam voluptatem saepe, assumenda odio possimus excepturi quam! Unde voluptas nemo dolorem quam libero, incidunt obcaecati rerum id iusto nam quae animi. Incidunt dicta libero excepturi earum dignissimos, animi doloribus. Blanditiis voluptatibus accusantium velit fugiat, perspiciatis repudiandae sapiente. Consequatur aliquam illum cupiditate iste. Cumque optio accusamus similique iure veritatis quam fugit voluptas molestiae laudantium velit sunt natus voluptate quidem possimus illo perspiciatis magnam, laboriosam provident labore earum aperiam saepe perferendis consequatur esse. Laudantium error dignissimos, blanditiis quod inventore commodi sed dolores tenetur! Deserunt sequi nemo cumque. Error libero quasi quos beatae, nulla voluptas voluptates quibusdam fuga animi veritatis cupiditate nemo laboriosam hic suscipit.</p>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo molestias minus delectus minima sed unde repudiandae ipsum! Officiis aliquam ab minus fugiat perspiciatis accusamus quaerat minima magnam. Deleniti, in eaque! Blanditiis, ab molestiae nemo beatae similique placeat veniam quo reiciendis odit quae neque sint iusto, ad magni aspernatur sapiente molestias dolorem illum vero iste quas doloremque! Voluptatibus nulla eveniet consectetur reprehenderit sit velit fugit!</p>
+                        <div class="event-image3"></div>
+                        <h3>Friday night Magic</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis illo, est fugit placeat et impedit explicabo adipisci neque sint natus voluptatum vero cupiditate ea laudantium quae! Asperiores molestias aspernatur laudantium. Ea voluptatibus nesciunt vero ducimus, saepe tempora officia minima rerum, culpa ut, molestiae esse amet provident! Soluta facilis maxime veritatis consequuntur numquam corrupti qui obcaecati reiciendis? Natus minus nihil voluptates obcaecati ab debitis, illum, molestias enim, praesentium quaerat quo veniam? Ab nam inventore harum officiis.</p>
+                        <div class="event-image4"></div>
+                        <h3>Star Wars Legion every saturday afternoon</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam animi, eaque magni itaque dolorem molestiae facilis debitis possimus dignissimos, nam laborum sequi porro consectetur sint eveniet dolores optio nihil non quos maxime? Suscipit quam alias voluptatem a, aut sequi reprehenderit quibusdam cupiditate debitis velit nulla! Corrupti, natus. Nam quae fugit laboriosam. Odio sit, earum veritatis necessitatibus, temporibus dolorem aliquam sapiente dolor itaque, ab aspernatur provident incidunt pariatur sed officia. Accusamus reiciendis soluta odit ea voluptatum, quisquam ipsam unde ex enim. At aspernatur sapiente saepe natus, ea numquam ad mollitia labore explicabo officia! Possimus, debitis nemo error magnam reiciendis maxime, velit, facere amet sint expedita et.</p>
+                    </div>
+                            </div>
+                        </div>
+                    </div>`;
+
+//this function is being called from the app.js and it has the page name
+export function modelPageName(pgName){
+    console.log(pgName);
+
+    $("#app").html(eval(pgName));
+}
